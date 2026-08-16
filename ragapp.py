f89182dd99e57665bb65963d2a43c865fa340cb0
Aditya Gupta <adigpt12@gmail.com>
2026-08-16 20:20:59 +0530

    Update ragapp.py

--- ragapp.py
+++ ragapp.py
@@ -5,8 +5,7 @@ from google import genai
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_community.document_loaders import Docx2txtLoader
 from langchain_community.vectorstores import FAISS
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 # =========================================================
 # PAGE CONFIG
